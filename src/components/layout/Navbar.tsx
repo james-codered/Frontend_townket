a3f8a47c2741b9416bb2d1a42a0fa21d9c26c043
james-codered <jamessamuelsamuel4@gmail.com>
2026-02-24 22:27:25 +0100

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           <img
             src="/logo.png"
             alt="CampusPreneur"
-            className="h-10 w-auto"
+            className="h-16 w-auto"
           />
         </Link>
 
